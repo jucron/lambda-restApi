3e204a62abbb49c7d238cd46fd5696109d81650c
Joao Renault <Joaomarcelo.Renault@emea.merkleinc.com>
2023-11-16 11:01:57 +0000

    first setup

--- bin/rest_api.ts
+++ bin/rest_api.ts
@@ -3,4 +3,8 @@ import * as cdk from 'aws-cdk-lib';
 import { RestApiStack } from '../lib/rest_api-stack';
 
 const app = new cdk.App();
-new RestApiStack(app, 'RestApiStack');
+const region = "eu-west-1";
+const account = "684807310606";
+new RestApiStack(app, 'RestApiStack',{
+    env: {region: region, account: account}
+});
